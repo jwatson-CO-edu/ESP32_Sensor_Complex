7b4297a00e6004cc06b82a7cb9db4b82726e8aea
James Watson <james.watson-2@colorado.edu>
2026-02-14 17:46:45 -0700

    Possibly correct example?

--- RockTumbler/ESP32C3_Transmitter/src/main.cpp
+++ RockTumbler/ESP32C3_Transmitter/src/main.cpp
@@ -1,35 +1,120 @@
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
+///// Includes ////////////////////////////////////////////////////////////
 #include <WiFi.h>
 #include <WebServer.h>
 #include "DHT.h"
 #include "WifiKey.h" // SECRETS HEADER FILE
 
-#define DHTTYPE DHT22   // DHT 22 (AM2302), AM2321
+///// Defines /////////////////////////////////////////////////////////////
+#define DEBUG_PRINT(x)   Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
 
-/* Put your SSID & Password */
+///// Constants ///////////////////////////////////////////////////////////
+bool _SERIAL_COMM = true;
+
+///// Globals /////////////////////////////////////////////////////////////
+float Temperature = 6e10;
+float Humidity    = 6e10;
+
+///// Temp / Humid Sensor Setup ///////////////////////////////////////////
+#define DHTTYPE DHT22   // DHT 22 (AM2302), AM2321
+uint8_t DHTPin = 1; // DHT Sensor pin // Digital Pin 01
+DHT     dht( DHTPin, DHTTYPE ); // Initialize DHT sensor.
+
+///// WLAN Setup //////////////////////////////////////////////////////////
 const char* ssid     = _SSID; // Enter your WiFi SSID
 const char* password = _PSWD; // Enter your WiFi password
-WebServer server(80); // Create a web server object that listens for HTTP requests on port 80
+WebServer   server(80); // Create a web server object that listens for HTTP requests on port 80
 
-// DHT Sensor pin
-uint8_t DHTPin = 1; // Digital Pin 01
+// Generate the HTML content to display
+String SendHTML( String Temperaturestat, String Humiditystat ) {
+    String ptr = "<!DOCTYPE html> <html>\n";
+    ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+    ptr += "<title>ESP32 Temperature & Humidity</title>\n";
+    ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+    ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+    ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+    ptr += "</style>\n";
+    ptr += "</head>\n";
+    ptr += "<body>\n";
+    ptr += "<div id=\"webpage\">\n";
+    ptr += "<h1>ESP32 Temperature & Humidity</h1>\n";
 
-// Initialize DHT sensor.
-DHT dht( DHTPin, DHTTYPE );
+    ptr += "<p>Temperature: ";
+    ptr += Temperaturestat;
+    ptr += " &deg;C</p>";
+    ptr += "<p>Humidity: ";
+    ptr += Humiditystat;
+    ptr += "%</p>";
+
+    ptr += "</div>\n";
+    ptr += "</body>\n";
+    ptr += "</html>\n";
+    return ptr;
+}
+
+// Handle connection to the root URL
+void handle_OnConnect() {
+    Temperature = dht.readTemperature(); // Get temperature value
+    Humidity    = dht.readHumidity(); // -- Get humidity value
+
+    // Check if any reading failed and send "N/A" if so
+    if( isnan( Temperature ) || isnan( Humidity ) ){
+        if( _SERIAL_COMM )  DEBUG_PRINTLN("Failed to read from DHT sensor!");
+        server.send(200, "text/html", SendHTML("N/A", "N/A"));
+    } else {
+        if( _SERIAL_COMM ){
+            DEBUG_PRINT( "Temperature: " );
+            DEBUG_PRINTLN( Temperature );
+            DEBUG_PRINT( "Humidity: " );
+            DEBUG_PRINTLN( Humidity );
+        }
+        
+        server.send( 
+            200, 
+            "text/html", 
+            SendHTML( String( Temperature ), String( Humidity ) ) ); // Send the HTML page with the values
+    }
+}
+
+// Handle not found URL
+void handle_NotFound() {
+    server.send( 404, "text/plain", "Not found" );    
+}
 
 
-// put function declarations here:
-int myFunction(int, int);
+
+////////// BOOT ////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    if( _SERIAL_COMM )  Serial.begin( 115200 ); // Start the Serial communication to send messages to the computer
+    delay( 100 );
+    dht.begin(); // Initialize the DHT sensor
+
+    if( _SERIAL_COMM )  DEBUG_PRINTLN( "Connecting to WiFi..." );
+    WiFi.begin( ssid, password ); // Connect to Wi-Fi network
+    // Wait until the device is connected to Wi-Fi
+    while( WiFi.status() != WL_CONNECTED ){
+        delay( 1000 );
+        DEBUG_PRINT(".");
+    }
+
+    DEBUG_PRINTLN("\nWiFi connected!");
+    DEBUG_PRINT("IP Address: ");
+    DEBUG_PRINTLN(WiFi.localIP()); // Print the IP address
+
+    server.on("/", handle_OnConnect); // Define handling function for root URL
+    server.onNotFound(handle_NotFound); // Define handling function for 404 (Not Found)
+
+    server.begin(); // Start the HTTP server
+    DEBUG_PRINTLN("HTTP server started");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+////////// MAIN ////////////////////////////////////////////////////////////////////////////////////
+
+void loop(){
+    server.handleClient(); // Handle client requests
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
